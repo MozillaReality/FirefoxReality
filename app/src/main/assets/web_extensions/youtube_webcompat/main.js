07afbb5728de2fe9437c9a30ff8b640d9da3d20d
Christopher Van Wiemeersch <hearcomestreble@gmail.com>
2019-03-29 19:10:47 -0700

    tidy up YouTube WebCompat WebExtension (follow up to PR #1047)

--- app/src/main/assets/web_extensions/youtube_webcompat/main.js
+++ app/src/main/assets/web_extensions/youtube_webcompat/main.js
@@ -1,8 +1,19 @@
-// Add meta-viewport
-let viewport = document.head.querySelector("meta[name='viewport']");
-if (!viewport) {
-    viewport = document.createElement("meta");
-    viewport.name = "viewport";
-    viewport.content = "width=user-width, initial-scale=1";
+(function () {
+  // If missing, inject a `<meta name="viewport">` tag to trigger YouTube's mobile layout.
+  const viewport = document.head.querySelector('meta[name="viewport"]');
+  if (!viewport) {
+    viewport = document.createElement('meta');
+    viewport.name = 'viewport';
+    viewport.content = 'width=user-width, initial-scale=1';
     document.head.appendChild(viewport);
-}
+  }
+
+  // Open the `Settings` menu.
+  document.querySelector('ytp-settings-button, .ytp-settings-button').click();
+
+  // Select the `Quality` sub-menu.
+  document.querySelector('ytp-settings-menu ytp-menuitem:last-child, .ytp-settings-menu .ytp-menuitem:last-child').click();
+
+  // Select the best `Quality`.
+  document.querySelector('ytp-quality-menu ytp-menuitem:first-child, .ytp-quality-menu .ytp-menuitem:first-child').click();
+})();
