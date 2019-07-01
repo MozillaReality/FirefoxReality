62754416fa92f583f219ffad211fd4837626c8cf
Manuel Martin <manmartin@mozilla.com>
2019-07-01 18:07:49 +0200

    Refactored session settings to SessionManager
    Always update all session when a session settings changes

--- app/src/common/shared/org/mozilla/vrbrowser/ui/widgets/settings/DisplayOptionsView.java
+++ app/src/common/shared/org/mozilla/vrbrowser/ui/widgets/settings/DisplayOptionsView.java
@@ -333,7 +333,7 @@ class DisplayOptionsView extends SettingsView {
         SettingsStore.getInstance(getContext()).setUaMode(checkId);
 
         if (doApply) {
-            SessionManager.get().getActiveStore().setUaMode((Integer)mUaModeRadio.getValueForId(checkId));
+            SessionManager.get().setUaMode((Integer)mUaModeRadio.getValueForId(checkId));
         }
     }
 
