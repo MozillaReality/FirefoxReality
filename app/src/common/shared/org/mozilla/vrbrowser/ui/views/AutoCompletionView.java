3bd2e4e2637e81e8d532a2268a33c72db9100c97
Imanol Fernandez <mortimergoro@gmail.com>
2019-07-09 13:01:12 +0200

    Fix autocompletion item overflow in the autocompletion bar

--- app/src/common/shared/org/mozilla/vrbrowser/ui/views/AutoCompletionView.java
+++ app/src/common/shared/org/mozilla/vrbrowser/ui/views/AutoCompletionView.java
@@ -7,7 +7,6 @@ package org.mozilla.vrbrowser.ui.views;
 
 import android.content.Context;
 import android.util.AttributeSet;
-import android.util.Log;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.FrameLayout;
