06bb3147c5c60052d94eceadfabc4d20726d45bd
Manuel Martin <manmartin@mozilla.com>
2019-10-03 14:28:00 +0200

    Tooltip fixes

--- app/src/common/shared/org/mozilla/vrbrowser/ui/widgets/TrayWidget.java
+++ app/src/common/shared/org/mozilla/vrbrowser/ui/widgets/TrayWidget.java
@@ -28,7 +28,6 @@ import org.mozilla.vrbrowser.browser.engine.SessionStore;
 import org.mozilla.vrbrowser.telemetry.TelemetryWrapper;
 import org.mozilla.vrbrowser.ui.views.UIButton;
 import org.mozilla.vrbrowser.ui.widgets.settings.SettingsWidget;
-import org.mozilla.vrbrowser.utils.SystemUtils;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -483,9 +482,9 @@ public class TrayWidget extends UIWidget implements SessionChangeListener, Windo
             mLibraryNotification.getPlacement().parentHandle = getHandle();
             mLibraryNotification.getPlacement().anchorY = 0.0f;
             mLibraryNotification.getPlacement().translationX = (offsetViewBounds.left + mBookmarksButton.getWidth() / 2.0f) * ratio;
-            mLibraryNotification.getPlacement().translationY = ((offsetViewBounds.top - 60) * ratio);
+            mLibraryNotification.getPlacement().translationY = ((offsetViewBounds.top - 35) * ratio);
             mLibraryNotification.getPlacement().translationZ = 25.0f;
-            mLibraryNotification.getPlacement().density = 3.0f;
+            mLibraryNotification.getPlacement().density = WidgetPlacement.floatDimension(getContext(), R.dimen.tray_tooltip_density);
             mLibraryNotification.setText(R.string.bookmarks_saved_notification);
             mLibraryNotification.setCurvedMode(false);
             mLibraryNotification.show(UIWidget.CLEAR_FOCUS);
