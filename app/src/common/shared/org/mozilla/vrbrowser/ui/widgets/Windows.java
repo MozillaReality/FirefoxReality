e1c35634079640427368face879bea4da522c62c
Manuel Martin <manmartin@mozilla.com>
2019-12-18 13:57:16 +0100

    FxA WebChannels implementation

--- app/src/common/shared/org/mozilla/vrbrowser/ui/widgets/Windows.java
+++ app/src/common/shared/org/mozilla/vrbrowser/ui/widgets/Windows.java
@@ -37,7 +37,6 @@ import java.io.Reader;
 import java.io.Writer;
 import java.lang.reflect.Type;
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
 
