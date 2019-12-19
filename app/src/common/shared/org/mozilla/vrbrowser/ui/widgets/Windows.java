09c262621e48bcbd69adc49ae333ac00345a8485
Manuel Martin <manmartin@mozilla.com>
2019-12-19 19:37:11 +0100

    Save state before restarting

--- app/src/common/shared/org/mozilla/vrbrowser/ui/widgets/Windows.java
+++ app/src/common/shared/org/mozilla/vrbrowser/ui/widgets/Windows.java
@@ -146,7 +146,7 @@ public class Windows implements TrayListener, TopBarWidget.Delegate, TitleBarWid
         restoreWindows();
     }
 
-    private void saveState() {
+    public void saveState() {
         File file = new File(mContext.getFilesDir(), WINDOWS_SAVE_FILENAME);
         try (Writer writer = new FileWriter(file)) {
             WindowsState state = new WindowsState();
