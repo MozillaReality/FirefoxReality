b7f3bb081ee147155230088deb23381b62f51134
Manuel Martin <manmartin@mozilla.com>
2019-12-19 15:10:18 +0100

    Add a setting to enable/disable tab restoration

--- app/src/common/shared/org/mozilla/vrbrowser/ui/widgets/Windows.java
+++ app/src/common/shared/org/mozilla/vrbrowser/ui/widgets/Windows.java
@@ -602,8 +602,9 @@ public class Windows implements TrayListener, TopBarWidget.Delegate, TitleBarWid
     }
 
     private void restoreWindows() {
+        boolean restoreEnabled = SettingsStore.getInstance(mContext).isRestoreTabsEnabled();
         WindowsState windowsState = restoreState();
-        if (windowsState != null) {
+        if (restoreEnabled && windowsState != null) {
             ArrayList<Session> restoredSessions = new ArrayList<>();
             if (windowsState.tabs != null) {
                 windowsState.tabs.forEach(state -> {
