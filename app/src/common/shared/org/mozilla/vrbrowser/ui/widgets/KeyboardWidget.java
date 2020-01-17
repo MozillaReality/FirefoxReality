aaafbc2c4a1da58bd5548865cf2c068be78cfdfa
Manuel Martin <manmartin@mozilla.com>
2020-01-17 15:29:54 +0100

    Indentation fix

--- app/src/common/shared/org/mozilla/vrbrowser/ui/widgets/KeyboardWidget.java
+++ app/src/common/shared/org/mozilla/vrbrowser/ui/widgets/KeyboardWidget.java
@@ -31,41 +31,41 @@ import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
 import org.mozilla.geckoview.GeckoSession;
 import org.mozilla.vrbrowser.R;
-import org.mozilla.vrbrowser.browser.engine.Session;
 import org.mozilla.vrbrowser.browser.SettingsStore;
+import org.mozilla.vrbrowser.browser.engine.Session;
 import org.mozilla.vrbrowser.input.CustomKeyboard;
 import org.mozilla.vrbrowser.telemetry.GleanMetricsService;
 import org.mozilla.vrbrowser.telemetry.TelemetryWrapper;
+import org.mozilla.vrbrowser.ui.keyboards.ChinesePinyinKeyboard;
+import org.mozilla.vrbrowser.ui.keyboards.ChineseZhuyinKeyboard;
 import org.mozilla.vrbrowser.ui.keyboards.DanishKeyboard;
-import org.mozilla.vrbrowser.ui.keyboards.FinnishKeyboard;
 import org.mozilla.vrbrowser.ui.keyboards.DutchKeyboard;
-import org.mozilla.vrbrowser.ui.keyboards.ItalianKeyboard;
+import org.mozilla.vrbrowser.ui.keyboards.EnglishKeyboard;
+import org.mozilla.vrbrowser.ui.keyboards.FinnishKeyboard;
 import org.mozilla.vrbrowser.ui.keyboards.FrenchKeyboard;
 import org.mozilla.vrbrowser.ui.keyboards.GermanKeyboard;
-import org.mozilla.vrbrowser.ui.keyboards.ChineseZhuyinKeyboard;
+import org.mozilla.vrbrowser.ui.keyboards.ItalianKeyboard;
 import org.mozilla.vrbrowser.ui.keyboards.JapaneseKeyboard;
 import org.mozilla.vrbrowser.ui.keyboards.KeyboardInterface;
+import org.mozilla.vrbrowser.ui.keyboards.KoreanKeyboard;
 import org.mozilla.vrbrowser.ui.keyboards.NorwegianKeyboard;
 import org.mozilla.vrbrowser.ui.keyboards.PolishKeyboard;
 import org.mozilla.vrbrowser.ui.keyboards.RussianKeyboard;
-import org.mozilla.vrbrowser.ui.keyboards.KoreanKeyboard;
 import org.mozilla.vrbrowser.ui.keyboards.SpanishKeyboard;
 import org.mozilla.vrbrowser.ui.keyboards.SwedishKeyboard;
 import org.mozilla.vrbrowser.ui.views.AutoCompletionView;
 import org.mozilla.vrbrowser.ui.views.CustomKeyboardView;
 import org.mozilla.vrbrowser.ui.views.KeyboardSelectorView;
 import org.mozilla.vrbrowser.ui.widgets.dialogs.VoiceSearchWidget;
-import org.mozilla.vrbrowser.ui.keyboards.ChinesePinyinKeyboard;
-import org.mozilla.vrbrowser.ui.keyboards.EnglishKeyboard;
 import org.mozilla.vrbrowser.utils.StringUtils;
 
 import java.util.ArrayList;
 import java.util.Locale;
-
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 
 
 public class KeyboardWidget extends UIWidget implements CustomKeyboardView.OnKeyboardActionListener, AutoCompletionView.Delegate,
@@ -376,18 +376,18 @@ public class KeyboardWidget extends UIWidget implements CustomKeyboardView.OnKey
 
     public void dismiss() {
         exitVoiceInputMode();
-       if (mFocusedView != null && mFocusedView != mAttachedWindow) {
-           mFocusedView.clearFocus();
-       }
-       mWidgetPlacement.visible = false;
-       mWidgetManager.updateWidget(this);
+        if (mFocusedView != null && mFocusedView != mAttachedWindow) {
+            mFocusedView.clearFocus();
+        }
+        mWidgetPlacement.visible = false;
+        mWidgetManager.updateWidget(this);
 
-       mWidgetManager.popBackHandler(mBackHandler);
+        mWidgetManager.popBackHandler(mBackHandler);
 
-       mIsCapsLock = false;
-       mIsLongPress = false;
-       handleShift(false);
-       hideOverlays();
+        mIsCapsLock = false;
+        mIsLongPress = false;
+        handleShift(false);
+        hideOverlays();
     }
 
     public void proxifyLayerIfNeeded(ArrayList<WindowWidget> aWindows) {
